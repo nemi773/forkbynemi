b5ebda07f95757aab5e0db08529265c1d31cd958
hp <hp@LAPTOP-14G6ACJ1>
2025-07-02 14:06:29 +0530

    Added Screenshot code

---
+++ Selenium_4_30/src/TakesScreeenshot/TakesScreenCode.java
@@ -0,0 +1,19 @@
+package TakesScreeenshot;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.io.FileHandler;
+
+public class TakesScreenCode {
+	public void getScreenPic(WebDriver driver) throws IOException
+	{
+		TakesScreenshot ts=(TakesScreenshot) driver;
+		File temp = ts.getScreenshotAs(OutputType.FILE);
+		File dest=new File("");
+		FileHandler.copy(temp, dest);
+	}
+}
